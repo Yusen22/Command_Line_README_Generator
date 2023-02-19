ed6132b3408956f5e90a469f3fe77febc222a8fd
Yusen22 <elijahwilsher@gmail.com>
2023-02-19 20:59:42 +0000

    added init function and writeToFile function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,26 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(questions) {
+  return `# ${questions.title}\n
+          ## ${questions.description}\n
+          ##Table of Contents\n
+          - [Installation](#installation)
+          - [Usage](#usage)
+          - [Credits](#credits)
+          - [Tests](#tests)
+          - [Questions](#questions)
+          - [Licence](#licence)\n
+          ## Installation\n 
+          ${questions.installation}\n
+          ## Usage 
+          ${questions.usage}\n
+          ## Contributors
+          ${questions.contributors}\n
+          ## Tests
+          ${questions.tests}\n
+          ## Questions
+          ${questions.questions}\n
+          ## Licence
+          ${questions.licence}\n
 
 `;
 }
